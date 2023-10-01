4a31ce761141958ce2481dfd77d98ed8bf3a8b4b
514sh <mark.joseph.balagtas@outlook.com>
2023-10-01 14:09:06 +0800

    added __init__.py for flask app

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from application.extensions import db
-from application.utils.config import Config
+from .extensions import db
+from .utils.config import Config
 
-from application.routes import users
+from .routes.users import blueprint as users_bp
 
 def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = Config.POSTGRES_URI
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-  app.register_blueprint(users.blueprint, url_prefix='/api/users')
+  app.register_blueprint(users_bp, url_prefix='/api/users')
   db.init_app(app)
 
   with app.app_context():
